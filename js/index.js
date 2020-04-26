6f8e7913b35ee33281afff554cb1fd5c49d70add
Gekagmeka <shulgagggeka@gmail.com>
2020-04-26 18:10:34 +0300

    modal is ready + mobile version

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('[data-toggle="open-modal"]').on('click', function(){
+	$('.modal').addClass('is-open');
+	$('body').addClass('lock');
+});
+$('[data-toggle="close-modal"]').on('click', function(){
+	$('.modal').removeClass('is-open');
+	$('body').removeClass('lock');
+});
